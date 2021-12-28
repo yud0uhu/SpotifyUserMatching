26dd0be98765001960534d1b6fc0b74a7a2fc8cf
yud0uhu <cist.b219.y.osaka@gmail.com>
2021-12-28 21:58:06 +0900

    fix: tree

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+import Button from "@mui/material/Button";
+
+function App() {
+  return <Button variant="contained">Hello World</Button>;
+}
+
+ReactDOM.render(<App />, document.querySelector("#app"));
