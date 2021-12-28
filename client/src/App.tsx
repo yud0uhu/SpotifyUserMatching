ae2627f8726486214413ce49c3c8e02aff66dbd2
yud0uhu <cist.b219.y.osaka@gmail.com>
2021-12-28 22:41:12 +0900

    remove: matelialui add: tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,3 @@
-import * as React from "react";
-import ReactDOM from "react-dom";
-import Button from "@mui/material/Button";
-
-function App() {
-  return <Button variant="contained">Hello World</Button>;
+export default function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
-
-ReactDOM.render(<App />, document.querySelector("#app"));
