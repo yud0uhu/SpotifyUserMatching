58ce2aae53474ad393cd932de8f2f7400c0fb73b
yud0uhu <cist.b219.y.osaka@gmail.com>
2021-12-29 11:45:12 +0900

    add: NavBar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { SpeakerphoneIcon, XIcon } from "@heroicons/react/outline";
 import { faCrown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Header from "./components/organisms/Header";
 
 export default function Example() {
   return (
-    <div className="bg-indigo-100 px-4 py-16 mx-auto sm:max-w-xl md:max-w-full md:px-24 lg:px-8 lg:py-20">
-      <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+    <div className="bg-indigo-100">
+      <Header />
+      <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12 px-4 py-16">
         <div className="bg-white box-content h-32 w-128 p-16 bg-white rounded-lg shadow-xl">
           <h1 className="m-4 Inter text-2xl text-gray-800 text-center">
             あなたの「好き」を見つける。
