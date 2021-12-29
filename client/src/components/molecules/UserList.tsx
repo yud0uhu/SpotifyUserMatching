083daa9579dc13560224e05cdce83999fcaf0ec3
yud0uhu <cist.b219.y.osaka@gmail.com>
2021-12-29 13:27:34 +0900

    fix: ユーザーリストのGRIDを修正

---
+++ client/src/components/molecules/UserList.tsx
@@ -0,0 +1,119 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { faCrown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+export default function UserList() {
+  return (
+    <>
+      <div className="text-center space-x-4 dark:text-gray-50 text-gray-900 text-lg">
+        <span>
+          <FontAwesomeIcon icon={faCrown} />
+        </span>
+        <span>USERさんと相性が合いそうなユーザー</span>
+      </div>
+      <div className="text-center space-x-4 text-xl space-y-12">
+        <h2 className="text-gray-500 dark:text-gray-400 text-sm leading-6 truncate">
+          相性20%マッチ
+        </h2>
+      </div>
+      <div className="grid grid-cols-3 gap-4 justify-items-auto">
+        <div className="text-gray-700 flex justify-center items-center px-4 py-4">
+          <div className="bg-white box-content h-32 w-128 p-16 bg-white rounded-lg shadow-xl">
+            <div className="flex">
+              <img
+                className="object-cover w-20 h-20 mr-4 rounded-full shadow"
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+                alt="Person"
+              />
+              <div className="flex flex-col justify-center">
+                <p className="text-lg font-bold">Neko</p>
+                <p className="text-sm text-gray-800">@dog_one</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="text-gray-700 flex justify-center items-center px-4 py-4">
+          <div className="bg-white box-content h-32 w-128 p-16 bg-white rounded-lg shadow-xl">
+            <div className="flex">
+              <img
+                className="object-cover w-20 h-20 mr-4 rounded-full shadow"
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+                alt="Person"
+              />
+              <div className="flex flex-col justify-center">
+                <p className="text-lg font-bold">Neko</p>
+                <p className="text-sm text-gray-800">@dog_one</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="text-gray-700 flex justify-center items-center px-4 py-4">
+          <div className="bg-white box-content h-32 w-128 p-16 bg-white rounded-lg shadow-xl">
+            <div className="flex">
+              <img
+                className="object-cover w-20 h-20 mr-4 rounded-full shadow"
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+                alt="Person"
+              />
+              <div className="flex flex-col justify-center">
+                <p className="text-lg font-bold">Neko</p>
+                <p className="text-sm text-gray-800">@dog_one</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="text-center space-x-4 text-xl space-y-12">
+        <h2 className="text-gray-500 dark:text-gray-400 text-sm leading-6 truncate">
+          相性15%マッチ
+        </h2>
+      </div>
+      <div className="grid grid-cols-3 gap-4 justify-items-auto h-48">
+        <div className="text-gray-700 flex justify-center items-center px-4 py-4">
+          <div className="bg-white box-content h-32 w-128 p-16 bg-white rounded-lg shadow-xl">
+            <div className="flex">
+              <img
+                className="object-cover w-20 h-20 mr-4 rounded-full shadow"
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+                alt="Person"
+              />
+              <div className="flex flex-col justify-center">
+                <p className="text-lg font-bold">Neko</p>
+                <p className="text-sm text-gray-800">@dog_one</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="text-gray-700 flex justify-center items-center px-4 py-4">
+          <div className="bg-white box-content h-32 w-128 p-16 bg-white rounded-lg shadow-xl">
+            <div className="flex">
+              <img
+                className="object-cover w-20 h-20 mr-4 rounded-full shadow"
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+                alt="Person"
+              />
+              <div className="flex flex-col justify-center">
+                <p className="text-lg font-bold">Neko</p>
+                <p className="text-sm text-gray-800">@dog_one</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="text-gray-700 flex justify-center items-center px-4 py-4">
+          <div className="bg-white box-content h-32 w-128 p-16 bg-white rounded-lg shadow-xl">
+            <div className="flex">
+              <img
+                className="object-cover w-20 h-20 mr-4 rounded-full shadow"
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+                alt="Person"
+              />
+              <div className="flex flex-col justify-center">
+                <p className="text-lg font-bold">Neko</p>
+                <p className="text-sm text-gray-800">@dog_one</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
