dc43069eb72ff6e9671bbc4686b4dc270ae7c3cf
yud0uhu <cist.b219.y.osaka@gmail.com>
2021-12-30 16:58:22 +0900

    fix: スキーマを修正

--- server/api/init_data.py
+++ server/api/init_data.py
@@ -1,9 +1,10 @@
-from models import engine, db_session, Base, User, Track
+from models import engine, db_session, Base, User
 from sqlalchemy.sql.expression import null
 Base.metadata.create_all(bind=engine)
 
-cat = User(id='123', user_name='cat')
+
+cat = User(id=123,twitter_id=321, user_name='cat')
 db_session.add(cat)
-dog = User(id='456', user_name='dog')
+dog = User(id=456,twitter_id=654, user_name='dog')
 db_session.add(dog)
 db_session.commit()
--- server/api/schema.py
+++ server/api/schema.py
@@ -1,34 +1,47 @@
 import graphene
 from graphene import relay
-from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
-from models import User as UserModel, Track as TrackModel
-
+from graphene_sqlalchemy import SQLAlchemyObjectType
+from models import User as UserModel
 
 class User(SQLAlchemyObjectType):
     class Meta:
         model = UserModel
-        interfaces = (relay.Node, )
+        # use `only_fields` to only expose specific fields ie "name"
+        # only_fields = ("name",)
+        # use `exclude_fields` to exclude specific fields ie "last_name"
+        # exclude_fields = ("last_name",)
 
-class UserConnections(relay.Connection):
-    class Meta:
-        node = User
+# class User(SQLAlchemyObjectType):
+#     class Meta:
+#         model = UserModel
+#         # interfaces = (relay.Node, )
+
+# class UserConnections(relay.Connection):
+#     class Meta:
+#         node = User
 
         
-class Track(SQLAlchemyObjectType):
-    class Meta:
-        model = TrackModel
-        interfaces = (relay.Node, )
+# class Track(SQLAlchemyObjectType):
+#     class Meta:
+#         model = TrackModel
+#         interfaces = (relay.Node, )
 
-class TrackConnections(relay.Connection):
-    class Meta:
-        node = Track
+# class TrackConnections(relay.Connection):
+#     class Meta:
+#         node = Track
+
+# class Query(graphene.ObjectType):
+#     node = relay.Node.Field()
+#     # Allows sorting over multiple columns, by default over the primary key
+#     all_users = SQLAlchemyConnectionField(UserConnections)
+#     # Disable sorting over this field
+#     all_tracks = SQLAlchemyConnectionField(TrackConnections, sort=None)
 
 class Query(graphene.ObjectType):
-    node = relay.Node.Field()
-    # Allows sorting over multiple columns, by default over the primary key
-    all_useers = SQLAlchemyConnectionField(UserConnections)
-    # Disable sorting over this field
-    all_tracks = SQLAlchemyConnectionField(TrackConnections, sort=None)
+    users = graphene.List(User)
 
+    def resolve_users(self, info):
+        query = User.get_query(info)  # SQLAlchemy query
+        return query.all()
 
 schema = graphene.Schema(query=Query)
