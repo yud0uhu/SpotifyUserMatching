15a1e6dc970e4ba688bdb14e0270f7406db46b92
yud0uhu <cist.b219.y.osaka@gmail.com>
2021-12-31 23:41:04 +0900

    fix: 1ユーザーあたり10曲・全10ユーザーのモックデータ流し込み

--- server/api/init_data.py
+++ server/api/init_data.py
@@ -23,37 +23,47 @@ def insert_track(user_id, track_id, track_name, audio, cover_art):
     db_session.commit()
 
 
-def insert_feature_track(energy, danceability, mode, acousticness, trackId):
+def insert_feature_track(energy, danceability, mode, acousticness, trackId, userId):
     
     feature_track = Track()
 
-    feature_track = FeatureTrack(energy=energy, danceability=danceability, mode=mode, acousticness=acousticness, track_id= trackId)
+    feature_track = FeatureTrack(energy=energy, danceability=danceability, mode=mode, acousticness=acousticness, track_id= trackId, user_id=userId)
 
     db_session.add(feature_track)
     db_session.commit()    
 
 
+userNameList = []
+userNameList = ["cat","dog","kuromame","datemaki","kurikinton","iwashi","kazunoko","konbumaki","dendukuri","ebi"]
 
-Id = 111
-twitterId = 111
-userName = "cat"
-init_session(Id, twitterId, userName)
+Id = 1
+twitterId = 1
+for userName in userNameList:
+    init_session(Id, twitterId, userName)
 
-track = spotify_connect.addTrackInf("Radiohead")
-# DBに登録する処理
-trackId=track['id']
-trackName=track['name']
-Audio=track['preview_url']
-CoverArt=track['album']['images'][0]['url']
-print(track)
+    trackNameList = []
+    trackNameList = ["All I Want for Christmas Is You","残響散歌","Answer","ハート","abcdefu","Afterglow","踊","Savage","Anniversary","勿忘"]
+    for trackNames in trackNameList:
+        print(trackNames)
 
-insert_track(user_id=Id, track_id=trackId, track_name=trackName, audio=Audio, cover_art=CoverArt)
+        track = spotify_connect.addTrackInf(trackNames)
+        # DBに登録する処理
+        trackId=track['id']
+        trackName=track['name']
+        Audio=track['preview_url']
+        CoverArt=track['album']['images'][0]['url']
+        # print(track)
 
-feature = spotify_connect.getAudioFeature(trackId)
-# DBに登録する処理
-energy = feature['energy']
-danceability = feature['danceability']
-mode = feature['mode']
-acousticness = feature['acousticness']
+        insert_track(user_id=Id, track_id=trackId, track_name=trackName, audio=Audio, cover_art=CoverArt)
 
-insert_feature_track(energy,danceability,mode,acousticness,trackId)
+        feature = spotify_connect.getAudioFeature(trackId)
+        # DBに登録する処理
+        energy = feature['energy']
+        danceability = feature['danceability']
+        mode = feature['mode']
+        acousticness = feature['acousticness']
+
+        insert_feature_track(energy,danceability,mode,acousticness,trackId,Id)
+    
+    Id +=1
+    twitterId += 1
