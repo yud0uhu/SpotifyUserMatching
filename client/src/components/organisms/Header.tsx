64ec7a23b1dafa0e49ab5563bb2986f545954d59
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-01-05 02:15:06 +0900

    fix: ヘッダーのルーティングを修正

--- client/src/components/organisms/Header.tsx
+++ client/src/components/organisms/Header.tsx
@@ -25,7 +25,7 @@ export default function Header() {
               <li className="nav-item">
                 <a
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="/mypage"
+                  href="/ranking"
                 >
                   <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i>
                   <span className="ml-2">My Page</span>
