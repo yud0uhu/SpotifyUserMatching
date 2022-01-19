d75ce826144657e66a3abc3f3fe49b61f5af50f6
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-01-20 08:37:16 +0900

    fix: .envを参照できるように修正

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+const API_KEY = import.meta.env.REACT_APP_AUTH0_DOMAIN;
+console.log(API_KEY);
+
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
-      domain={process.env.REACT_APP_AUTH0_DOMAIN!}
-      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID!}
-      audience={process.env.REACT_APP_AUTH0_AUDIENCE!}
-      scope={process.env.REACT_APP_AUTH0_SCOPE!}
+      domain={import.meta.env.REACT_APP_AUTH0_DOMAIN!}
+      clientId={import.meta.env.REACT_APP_AUTH0_CLIENT_ID!}
+      audience={import.meta.env.REACT_APP_AUTH0_AUDIENCE!}
+      scope={import.meta.env.REACT_APP_AUTH0_SCOPE!}
       redirectUri={window.location.origin}
     >
       <App />
